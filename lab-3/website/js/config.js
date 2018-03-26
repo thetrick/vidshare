751788a0478a84250c1404ccd2e69fb8df9ca60a
Todd G. Hetrick <toddghetrick@gmail.com>
2018-03-26 14:44:48 -0400

    scrubbed for safety

--- lab-3/website/js/config.js
+++ lab-3/website/js/config.js
@@ -3,9 +3,5 @@ var configConstants = {
         domain: '##ENTER_YOUR_OWN##.auth0.com',
         clientId: '##ENTER_YOUR_OWN##'
     },
-<<<<<<< HEAD
-    apiBaseUrl: 'https://api.awspractice.com/v1'
-=======
     apiBaseUrl: '##ENTER_YOUR_OWN##'
->>>>>>> a19548b8cc9100c7d0b9eb5f6a09e265885a0c69
 };
